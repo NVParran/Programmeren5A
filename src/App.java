7d79cee44904a9608d1b773a9c09e44eaa056716
erikw <1033805@hr.nl>
2022-10-18 13:38:34 +0200

    juamn toegeboegf

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("test");
+        System.out.println("juanmoment");
     }
 }
