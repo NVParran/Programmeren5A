a59e324589db42314c231ce5f6ed5a9aeb233824
eliam <eliamtraas@gmail.com>
2022-10-18 13:47:56 +0200

    kale minols

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ public class App {
         System.out.println("test");
         System.out.println("juanmoment");
         System.out.println("InitailD");
+        System.out.println("no dick no balls");
     }
 }
