24b497deb3cc82335aa2cf23efa8eaf76105286f
eliam <eliamtraas@gmail.com>
2022-10-27 20:08:08 +0200

    test

--- src/App.java
+++ src/App.java
@@ -4,7 +4,5 @@ public class App {
         System.out.println("test");
         System.out.println("juanmoment");
         System.out.println("InitailD");
-        System.out.println("no dick no balls");
-
     }
 }
