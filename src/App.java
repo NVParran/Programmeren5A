0310543208d07cf4e629043fc984101632493dd6
NVPARRAN <n.vparran@gmail.com>
2022-10-30 14:12:49 +0100

    Mitsubishi & Toyota

--- src/App.java
+++ src/App.java
@@ -1,6 +1,4 @@
 public class App {
     public static void main(String[] args) {
-        MitsubishiLanEvo3 car = new MitsubishiLanEvo3("Mitsubishi",1990, new Motor("Epicmotor"),"Rood", "IeatS");
-        System.out.println(car.getdetails());
     }
 }
--- src/MitsubishiAuto.java
+++
@@ -1,7 +0,0 @@
-//public class MitsubishiAuto extends AbstractAuto {
-//    public String merk;
-//    public int jaar;
-//    public Motor motor;
-//    public String kleur;
-//    public String nummerplaat;
-//}
